a179e6832525db00525914f67a941a6cab9d7181
Lautaro Vazquez <lauti1498@gmail.com>
2022-08-09 03:34:08 -0300

    correcion del proyecto

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-
-const motos = [
-    {id:1, marca: "Honda", modelo: "CRF 300L", precio: 659375, imagen: "assets/imgs/honda/crf300l.jpg"},
-    {id:2, marca: "Honda", modelo: "CRF 250R", precio: 1102960, imagen:"assets/imgs/honda/CRF250R.jpg"},
-    {id:3, marca: "Honda", modelo: "CRF 450R", precio: 1162900, imagen: "assets/imgs/honda/CRF450R.jpg"},
-    {id:4, marca: "Kawasaki", modelo: "KLX 250L", precio: 548482, imagen: "assets/imgs/kawasaki/klx250l.jpg"},
-    {id:5, marca: "Kawasaki", modelo: "KX 250F", precio: 1060990, imagen: "assets/imgs/kawasaki/kx250f.jpg"},
-    {id:6, marca: "Kawasaki", modelo: "KX 450F", precio: 1126936, imagen: "assets/imgs/kawasaki/kx450f.jpg"},
-    {id:7, marca: "KTM", modelo: "EXC - F350", precio: 1192380, imagen: "assets/imgs/ktm/exc-f350.jpg"},
-    {id:8, marca: "KTM", modelo: "SX 125", precio: 1039420, imagen: "assets/imgs/ktm/sx125.jpg"},
-    {id:9, marca: "KTM", modelo: "SX 450", precio: 1322352, imagen: "assets/imgs/ktm/sx450.jpg"},
-    {id:10, marca: "Suzuki", modelo: "RM-Z 250", precio: 1001055, imagen: "assets/imgs/suzuki/rm-z250.jpg"},
-    {id:11, marca: "Suzuki", modelo: "RM-Z 450", precio: 1018670, imagen: "assets/imgs/suzuki/rm-z450.jpg"},
-    {id:12, marca: "Suzuki", modelo: "RM-Z 450 2017", precio: 959095, imagen: "assets/imgs/suzuki/rm-z4502017.jpg"},
-    {id:13, marca: "Yamaha", modelo: "WR 450", precio: 1198870, imagen: "assets/imgs/yamaha/wr450f.jpg"},
-    {id:14, marca: "Yamaha", modelo: "YZ 250", precio: 995060, imagen: "assets/imgs/yamaha/yz250.jpg"},
-    {id:15, marca: "Yamaha", modelo: "YZ 450", precio: 1155095, imagen: "assets/imgs/yamaha/yz450f.jpg"}
-]
 let carrito = []
 const contenedorMotos = document.getElementById('contenedorMotos')
 const carritoItem = document.getElementById('carritoItem')
@@ -27,38 +9,61 @@ document.addEventListener('DOMContentLoaded', () => {
         carrito = JSON.parse(localStorage.getItem('carrito'))
         actualizarCarrito()
         pagarCarrito()
+        fetchData()
     }
 })
 
 
+//Funcíon para tomar los datos del archivo .json
+const fetchData = async () => {
+    const resp = await fetch('data.json')
+    const data = await resp.json()
+    // console.log(data)
+    pintarData(data)
+}
 
-motos.forEach(producto => {
-    const div = document.createElement('div')
-    div.classList.add('moto')
-    div.innerHTML = `
-                        <img class="itemImagen" src="${producto.imagen}" alt="">
-                        <h2 class="itemTitulo">${producto.marca}</h2>
-                        <h3 class="itemModelo">${producto.modelo}</h3>
-                        <p class="itemPrecio">$${producto.precio}</p>
-                        <button id="boton${producto.id}" class="itemBoton">COMPRAR</button>
-                        `
-    
-    contenedorMotos.appendChild(div)
+//Función para mostrar los productos de forma dinámica en el DOM
+const pintarData = (data) => {
+    data.forEach(producto => {
+        const div = document.createElement('div')
+        div.classList.add('moto')
+        div.innerHTML = `
+                            <img class="itemImagen" src="${producto.imagen}" alt="">
+                            <h2 class="itemTitulo">${producto.marca}</h2>
+                            <h3 class="itemModelo">${producto.modelo}</h3>
+                            <p class="itemPrecio">$${producto.precio}</p>
+                            <button id="boton${producto.id}" class="itemBoton">COMPRAR</button>
+                            `
+        
+        contenedorMotos.appendChild(div)
 
-    const boton = document.getElementById(`boton${producto.id}`)
-    boton.addEventListener('click', () => {
+        const boton = document.getElementById(`boton${producto.id}`)
+        boton.addEventListener('click', () => {
         agregarAlCarrito(producto.id)
-    })
-    
-    })
+})
+})
+}
 
+//Función para agregar elementos al carrito
 const agregarAlCarrito = (motoId) => {
-    const item = motos.find((moto)=> moto.id === motoId)
-    carrito.push(item)
+    const repetido = carrito.some (prod => prod.id === motoId)
+    if (repetido) {
+        const prod = carrito.map(prod => {
+            if (prod.id === motoId) {
+                prod.cantidad++
+
+            }
+        } )
+    } else {
+        const item = motos.find((moto)=> moto.id === motoId)
+        carrito.push(item)
+    }
+    
     actualizarCarrito()
     // console.log(carrito)
 }
 
+//Funcíon para borrar elementos del carrito
 const borrarDelCarrito = (prodId) => {
     const item = carrito.find((prod)=>prod.id === prodId)
     const indice = carrito.indexOf(item)
@@ -66,6 +71,7 @@ const borrarDelCarrito = (prodId) => {
     actualizarCarrito()
 }
 
+//Funcíon que actualiza el carrito cada vez que se realice algo en él
 const actualizarCarrito = () => {
     carritoItem.innerHTML = ""
     carrito.forEach((prod) => {
@@ -75,17 +81,20 @@ const actualizarCarrito = () => {
                         <p class="itemTitulo">${prod.marca}</p>
                         <p class="itemModelo">${prod.modelo}</p>
                         <p class="itemPrecio">${prod.precio}</p>
+                        <p class="itemCantidad">${prod.cantidad}</p>
                         <button onclick= "borrarDelCarrito(${prod.id})" class="botonBorrar">X</button>`
     carritoItem.appendChild(div)
 
     localStorage.setItem('carrito', JSON.stringify(carrito))
     })
-    precioTotal.innerText = "$" + carrito.reduce((acc, prod) => acc + prod.precio, 0)
+    precioTotal.innerText = "$" + carrito.reduce((acc, prod) => acc + prod.precio*prod.cantidad, 0)
 }
 
+//Función para que al apretar el boton comprar salgan las alertas correspondientes
 const pagarCarrito = () => {
     pagar.addEventListener('click', () => {
-        Swal.fire({
+        if (carrito.length != 0) {
+            Swal.fire({
             icon: 'success',
             title: '¡Gracias por su compra!',
             showConfirmButton: false,
@@ -93,5 +102,14 @@ const pagarCarrito = () => {
         })
         carrito.length = 0
         actualizarCarrito()
+        } else {
+            Swal.fire ({
+                icon: 'error',
+                title: '¡Su carrito de compras esta vacio!',
+                showConfirmButton: false,
+                timer: 1500
+            })
+        }
     })
+    
 }
